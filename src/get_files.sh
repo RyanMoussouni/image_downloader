a5238b773945ed4074cef5d59fc011806d6614a8
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2021-06-08 11:24:52 +0200

    added folder check, updated script to match new link file structure

--- src/get_files.sh
+++ src/get_files.sh
@@ -1,39 +1,33 @@
 #!/bin/bash
 ###### VARIABLES ######
 folder=""
-query=""
 idx=0
 
 
 ##### ROUTINES DEFINITION #####
 function update_idx(){
-	idx=$((idx+ 1))
+	idx=$((10#${idx} + 1))
 }
 
-function get_query(){
-	local filename=$1
-	local parts=(${filename//_/ })
-	query=${parts[1]}
+function pad_idx(){
+	local temp="00000${idx}"
+	idx=${temp:(-5):5}
 }
-
-function get_folder(){
-	local parts=(${query//+/ })
-	folder="${parts[0]}_${parts[1]}"
-}
-
 
 ##### BODY #####
 cat links.txt | while read line; do   
-	get_query $line
-	echo ${query}
+	folder="${line[0]}"
+	url="${line[1]}"
 
-	if [[ ${idx} -eq 0 ]]; then
-		get_folder
-		echo "passed"
+	if [ -d "../data/images/raw/${folder}" ]; then
+		curl --output "../data/images/raw/${folder}/image_${idx}.jpg" --url ${url} 
+	else
+		mkdir "../data/images/raw/${folder}"
+		curl --output "../data/images/raw/${folder}/image_${idx}.jpg" --url ${url}
 	fi
-	#curl --output "../data/images/raw/image_${idx}.jpg" --url $line; 
-	
+
 	update_idx
+	pad_idx
 
 	echo ${idx}
 done
