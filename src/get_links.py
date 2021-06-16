00f97b2041556425521bf7761bea43617dcdabdb
Ryan Moussouni <ryan.moussouni@telecom-paristech.fr>
2021-06-16 17:25:37 +0200

    Added Error handling

--- src/get_links.py
+++ src/get_links.py
@@ -18,11 +18,16 @@ PATH = "../data/links/api_responses/"
 with open("links.txt", "w") as f:
 	files = os.listdir(PATH)
 	for file in files:
-		name, surname = file.split("_")[1].split("+")[0], file.split("_")[1].split("+")[1]
-		for link in get_link_gen(file):
-			f.write("{0}_{1} {2}\n".format(name, surname, link))
+		try :
+			name, surname = file.split("_")[1].split("+")[0], file.split("_")[1].split("+")[1]
+			for link in get_link_gen(file):
+				f.write("{0}_{1} {2}\n".format(name, surname, link))
+		except IndexError:
+			pass
 	f.close()
 	
 	for file in files:
-		os.remove(PATH + file)
-	
+		try:
+			os.remove(PATH + file)
+		except IsADirectoryError:
+			pass
